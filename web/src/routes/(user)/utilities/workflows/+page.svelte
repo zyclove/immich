c9e03003c0e2f516331eb45fef84032bc4438e0d
Alex Tran <alex.tran1502@gmail.com>
2024-12-28 23:09:24 -0600

    wip: update

--- web/src/routes/(user)/utilities/workflows/+page.svelte
+++ web/src/routes/(user)/utilities/workflows/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
   import UserPageLayout from '$lib/components/layouts/user-page-layout.svelte';
   import type { PageData } from './$types';
-  import UtilitiesMenu from '$lib/components/utilities-page/utilities-menu.svelte';
 
-  export let data: PageData;
+  interface Props {
+    data: PageData;
+  }
+
+  let { data }: Props = $props();
 </script>
 
 <UserPageLayout title={data.meta.title}>
-  <div class="w-full max-w-xl m-auto"></div>
+  <div class="w-full h-full bg-gray-50 dark:bg-immich-dark-gray rounded-xl p-6">hello</div>
 </UserPageLayout>
