4e33f9944df601ec230a056a5aad3468fd3a01d5
Jason Rasmussen <jason@rasm.me>
2025-01-23 17:33:06 -0500

    chore: tweaks

--- web/src/lib/components/layouts/AuthPageLayout.svelte
+++ web/src/lib/components/layouts/AuthPageLayout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { Card, CardBody, CardHeader, Heading, Logo, VStack } from '@immich/ui';
+  import { Card, CardBody, CardHeader, Heading, immichLogo, Logo, VStack } from '@immich/ui';
   import type { Snippet } from 'svelte';
 
   interface Props {
@@ -11,9 +11,11 @@
 </script>
 
 <section class="min-w-screen flex min-h-dvh items-center justify-center relative">
-  <div class="absolute -z-10 w-[99%] h-[99%] flex place-items-center place-content-center">
-    <img src="/immich-logo-no-bg.svg" class="w-[750px] mb-2 antialiased -z-10" alt="Immich logo" />
-    <div class="w-full h-[99%] absolute left-0 top-0 backdrop-blur-3xl bg-immich-bg/80 dark:bg-immich-dark-bg/20"></div>
+  <div class="absolute -z-10 w-full h-full flex place-items-center place-content-center overflow-hidden">
+    <img src={immichLogo} class="w-[750px] py-8 mb-2 antialiased" alt="Immich logo" />
+    <div
+      class="w-full h-full absolute left-0 top-0 backdrop-blur-[200px] bg-immich-bg/80 dark:bg-immich-dark-bg/20"
+    ></div>
   </div>
 
   <Card color="secondary" class="w-full max-w-lg border m-2">
